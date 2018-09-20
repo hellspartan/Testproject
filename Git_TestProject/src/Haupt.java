fea1a6dff6ce18408913a8bd65561c379b447986
Sebastian <Sebastian@Sebastian-PC.fritz.box>
2018-09-20 14:10:32 +0200

    Hauptklasse veraendert

--- Git_TestProject/src/Haupt.java
+++ Git_TestProject/src/Haupt.java
@@ -3,8 +3,11 @@ public class Haupt {
 
 	public static void main(String[] args) {
 		
+		private int d;
+		
 		System.out.println("Hallo Welt!");
 		System.out.println("Hier wurde etwas ergänzt.");
+		
 	}
 
 }
